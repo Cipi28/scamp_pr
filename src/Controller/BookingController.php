d79ca6d7f3b7c4b0c9ed9c3b82c2e1299e67b767
cipi <ciprian.manea2@student.upt.ro>
2022-07-09 19:35:39 +0300

    added booking form and style

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -2,25 +2,44 @@
 
 namespace App\Controller;
 
+use App\Entity\Booking;
 use App\Entity\Car;
+use App\Form\BookingForm;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BookingController extends AbstractController
 {
     #[Route('/booking/{car_id}', name: 'app_booking')]
-    public function read($car_id, EntityManagerInterface $entityManager): Response
+    public function read($car_id, EntityManagerInterface $entityManager, Request $request): Response
     {
 
         $car = $entityManager->getRepository(Car::class)->find($car_id);
 //        return $this->render('booking/index.html.twig', [
 //            'car' => $car,
 //        ]);
+        $booking = new Booking();
+        $form = $this->createForm(BookingForm::class, $booking);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $booking->setStartTime($form->get('startTime')->getData());
+            $booking->setDuration($form->get('duration')->getData());
+
+            $entityManager->persist($booking);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $this->redirectToRoute('user');
+        }
 
         return $this->render('base.html.twig',[
             'car' => $car,
+            'BookingForm' => $form->createView(),
             'template' => 'booking/index.html.twig'
         ]);
     }
---
+++ src/Form/BookingForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Booking;
+use Doctrine\DBAL\Types\TextType;
+use Doctrine\DBAL\Types\TimeType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use \Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+
+
+
+class BookingForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('startTime', DateTimeType::class)
+            ->add('duration', \Symfony\Component\Form\Extension\Core\Type\TimeType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Booking::class,
+        ]);
+    }
+}
